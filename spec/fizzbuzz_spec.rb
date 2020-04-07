ca31f356bac0ee05e5b7cf5fca313a78df2298d2
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-04-07 14:55:08 +0000

    Amend test cases in spec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,11 +6,11 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
   
-  it "return 'buzz' when passed 5" do
+  it "return 'buzz' when number passed divides by 5" do
     expect(fizzbuzz(5)).to eq "buzz"
   end 
   
-  it "return 'fizzbuzz' when passed 15" do 
+  it "return 'fizzbuzz' when number passed divides by 5 & 3" do 
     expect(fizzbuzz(15)).to eq "fizzbuzz"
   end
   
