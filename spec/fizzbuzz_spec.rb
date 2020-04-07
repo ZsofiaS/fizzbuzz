0fca16eeaa1f342288c3f629d00abaadf93d0fb0
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-04-07 14:20:11 +0000

    Amended fizzbuzz - add modulo

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,7 +2,7 @@ require "fizzbuzz"
 
 describe 'fizzbuzz' do
     
-  it 'return "fizz" when passed 3' do
+  it 'return "fizz" when number passed divides by 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
   
